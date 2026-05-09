41cce64149130fb25f0eee6097fa08c2d03a5c2e
Aazad Guragai <aazadguragai00@gmail.com>
2026-05-09 12:52:51 -0400

    third

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,17 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import TicTacToe from "./pages/TicTacToe";
-import MemoryGame from "./pages/Memogame";
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tic-tac-toe" element={<TicTacToe />} />
-        <Route path="/memory-game" element={<Memogame />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <h1>Game Hub</h1>
+    </>
   );
 }
 
